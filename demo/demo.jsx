d34ccf1e4444cd9535831d0e929bb2b54821b753
Mark Steve Samson <hello@marksteve.com>
2015-02-26 22:19:36 +0800

    Add demo for Toggable

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -2,12 +2,51 @@ var React = require('react')
 
 var rc = require('../')
 
-React.render(
-  <rc.SuffixedInput
-    className="rc-suffixed-input"
-    suffix="apples"
-    defaultValue="99"
-  />,
-  document.getElementById('suffixed-input')
-)
+var ToggableDemo = React.createClass({
+  getInitialState() {
+    return {
+      toggled: false
+    }
+  },
+  render() {
+    return (
+      <div>
+        <rc.Toggable
+          className="rc-toggable"
+          toggled={this.state.toggled}
+        />
+        <button onClick={this._toggle}>
+          Toggle
+        </button>
+      </div>
+    )
+  },
+  _toggle() {
+    this.setState({toggled: !this.state.toggled})
+  },
+})
 
+var Demo = React.createClass({
+  render() {
+    return (
+      <div>
+
+        <h3 id="toggable">Toggable</h3>
+        <ToggableDemo />
+
+        <h3 id="suffixed-input">Suffixed Input</h3>
+        <rc.SuffixedInput
+          className="rc-suffixed-input"
+          suffix="%"
+          defaultValue="99"
+        />
+
+      </div>
+    )
+  }
+})
+
+React.render(
+  <Demo />,
+  document.getElementById('demo')
+)
